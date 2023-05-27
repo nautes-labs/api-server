8f49dd8a37be9da177d8701ca8cf6c42f1822779
luojinxu <382967255@qq.com>
2023-05-28 01:01:04 +0800

    feat: modify gitlab ca path.

--- pkg/gitlab/gitlab.go
+++ pkg/gitlab/gitlab.go
@@ -25,9 +25,9 @@ import (
 )
 
 const (
-	_CaCertPath        = "/usr/local/share/ca-certificates/ca.crt"
-	_ApiServerCertPath = "/usr/local/share/ca-certificates/apiserver.crt"
-	_ApiServerKeyPath  = "/usr/local/share/ca-certificates/apiserver.key"
+	_CaCertPath        = "/opt/nautes/out/pki/ca-certificates/ca.crt"
+	_ApiServerCertPath = "/opt/nautes/out/pki/ca-certificates/apiserver.crt"
+	_ApiServerKeyPath  = "/opt/nautes/out/pki/ca-certificates/apiserver.key"
 )
 
 type GitlabClient struct {
