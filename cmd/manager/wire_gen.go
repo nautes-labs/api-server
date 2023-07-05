1758868a4c668e55c78b50fea9c3aa4596b9bfb2
luojinxu <382967255@qq.com>
2023-07-05 13:39:03 +0800

    feat: inject resource case.

--- cmd/manager/wire_gen.go
+++ cmd/manager/wire_gen.go
@@ -20,6 +20,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+import (
+	_ "net/http/pprof"
+)
+
 // Injectors from wire.go:
 
 // wireApp init kratos application.
@@ -45,7 +49,7 @@ func wireApp(confServer *conf.Server, logger log.Logger, nodesTree nodestree.Nod
 	projectPipelineRuntimeUsecase := biz.NewProjectPipelineRuntimeUsecase(logger, codeRepo, nodesTree, resourcesUsecase, client2, config)
 	projectPipelineRuntimeService := service.NewProjectPipelineRuntimeService(projectPipelineRuntimeUsecase, resourcesUsecase)
 	deploymentRuntimeUsecase := biz.NewDeploymentRuntimeUsecase(logger, codeRepo, nodesTree, resourcesUsecase, client2, config)
-	deploymentruntimeService := service.NewDeploymentruntimeService(deploymentRuntimeUsecase)
+	deploymentruntimeService := service.NewDeploymentruntimeService(deploymentRuntimeUsecase, resourcesUsecase)
 	codeRepoService := service.NewCodeRepoService(codeRepoUsecase, config)
 	codeRepoBindingService := service.NewCodeRepoBindingService(codeRepoBindingUsecase, resourcesUsecase)
 	projectUsecase := biz.NewProjectUsecase(logger, codeRepo, secretrepo, nodesTree, config, resourcesUsecase)
